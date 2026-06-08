8e40a28d1a74307b557f2a9c151b8f84181be1a6
iant1098 <123456789+iantan2476@gmail.com>
2026-06-09 02:37:04 +0800

    Set bottom nav height to 55px

--- src/App.tsx
+++ src/App.tsx
@@ -499,7 +499,7 @@ export default function App() {
             </div>
           )}
 
-          <div className="relative z-10 h-full overflow-y-auto overscroll-contain pb-14 text-left scrollbar-none">
+          <div className="relative z-10 h-full overflow-y-auto overscroll-contain pb-[63px] text-left scrollbar-none">
             {currentTab === 1 && (
               <HomeCombinedTab
                 activeUserId={activeUserId}
@@ -570,7 +570,7 @@ export default function App() {
             />
           )}
 
-          <nav className="fixed bottom-0 left-1/2 z-[60] flex h-12 w-full max-w-[480px] -translate-x-1/2 items-center justify-around border-t border-[#6f6d6c]/15 bg-black px-2 shadow-[0_-6px_18px_rgba(0,0,0,0.45)]">
+          <nav className="fixed bottom-0 left-1/2 z-[60] flex h-[55px] w-full max-w-[480px] -translate-x-1/2 items-center justify-around border-t border-[#6f6d6c]/15 bg-black px-2 shadow-[0_-6px_18px_rgba(0,0,0,0.45)]">
             <button
               type="button"
               onClick={() => {
